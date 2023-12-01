762fff1f75034554a18e23f6217530e814257664
Mahmudur Rahman Tasin <mtasin2017@gmail.com>
2023-12-01 19:27:35 +0600

    Create nested1.c

---
+++ subeen1/nested1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    int i,j;
+
+    for (i=0;i<2;i++)
+    {
+        printf ("out\n",i);
+
+        for(j=0;j<2;j++)
+        {
+            printf ("in\n",j);
+
+        }
+
+    }
+    getch();
+    return 0;
+}
