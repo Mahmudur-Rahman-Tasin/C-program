472ef374ae8a467887354dd8532e8244e80c890c
Mahmudur Rahman Tasin <mtasin2017@gmail.com>
2023-12-01 19:01:23 +0600

    Create quadraticeq.c

---
+++ quadraticeq.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+
+//Suppose, given quadratic equation : ax^2+bx+c=0
+
+int main()
+{
+    float a,b,c;
+    scanf("%f %f %f",&a,&b,&c);
+
+    float x1,x2;
+    x1=(-b+sqrt(pow(b,2)-4*a*c))/(2*a);
+    x2=(-b-sqrt(pow(b,2)-4*a*c))/(2*a);
+
+    printf("x1: %.2f\nx2: %.2f",x1,x2);
+    return 0;
+}
