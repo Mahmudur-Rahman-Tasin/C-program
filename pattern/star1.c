5dcc7c1f61755c953a6ad300b198b57503beec4d
Mahmudur Rahman Tasin <mtasin2017@gmail.com>
2023-12-01 19:24:19 +0600

    Update star1.c

--- pattern/star1.c
+++ pattern/star1.c
@@ -1,1 +1,26 @@
+#include<stdio.h>
 
+int main()
+{
+    int n,m,j,i;
+    printf("pashapashi : ");
+    scanf ("%d",&n);
+
+
+    printf("lombalombi : ");
+    scanf ("%d",&m);
+
+    for(j=1;j<=m;j++)
+    {
+        for(i=1;i<=n;i++)
+        {
+            printf("*");
+        }
+        printf("\n");
+
+    }
+
+
+    getch();
+    return 0;
+}
