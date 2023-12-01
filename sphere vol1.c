c140de57d887291c0ddbfceb755304bf26fba08c
Mahmudur Rahman Tasin <mtasin2017@gmail.com>
2023-12-01 15:11:34 +0600

    Create sphere vol1.c

---
+++ sphere vol1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+int r; double v; double pi=22/7.0;
+printf("Radius of sphere=");
+scanf("%d",&r);
+v=(4/3.0)*pi*r*r*r;//int division
+printf("Volume of sphere=%lf",v);
+
+return 0;
+}
