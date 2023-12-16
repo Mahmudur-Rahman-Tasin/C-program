d907f9c3dbe5e98693bf42a4e32a3df107041d0c
Atika-Alam <152618425+Atika-Alam@users.noreply.github.com>
2023-12-16 21:21:13 +0600

    Add files via upload

---
+++ loop/Fibonacci.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main()
+{
+    int first=0, second=1, count=0, fibo, n;
+
+    printf("Enter number of terms: ");
+    scanf("%d", &n);
+
+    for(;count<n;)
+    {
+        if(count<=1)
+        {
+            fibo=count;
+        }
+
+        else
+        {
+            fibo=first+second;
+            first=second;
+            second=fibo;
+        }
+
+
+
+        printf("%d  ",fibo);
+        count++;
+    }
+}
