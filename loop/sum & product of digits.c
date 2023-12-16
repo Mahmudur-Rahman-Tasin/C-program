9c2cb80d6fb52b6324f9a2cc99e79c032391f843
Atika-Alam <152618425+Atika-Alam@users.noreply.github.com>
2023-12-16 17:19:50 +0600

    sum & product of digits.c

--- loop/sum & product of digits.c
+++ loop/sum & product of digits.c
@@ -3,15 +3,17 @@
 
 int main()
 {
-    int n,r,d=0,sum;
+    int n,d=0,sum, product=1;
     scanf("%d",&n);
     for(;n!=0;) //or n>0 or while
     {   
         d=n%10; //remainder is last digit
         sum=sum+d;
+        product=product*d;
         n=n/10; //excludes last digit from number
 
     }
-    printf("%d",sum);
+    printf("Sum: %d\n",sum);
+    printf("Product: %d\n",product);
     return 0;
 }
