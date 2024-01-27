a996710742fea915a5eb91ae1884c912a0ae2093
Mahmudur-Rahman-Tasin <mtasin2017@gmail.com>
2024-01-27 13:10:38 +0600

    Create freq.c

---
+++ array/hw/freq.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int n=12,i,j;
+    int a[]={0,1,2,3,4,5,5,7,7,8,9,10};
+    int f[n];
+    for(i=0;i<n;i++)
+    {
+        f[i]=0;
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            if(a[j]==a[i]) f[i]++;
+        }
+    }
+    for(i=0;i<n;i++)
+        {
+            if(f[i]>1) continue;
+            printf("%d occurs %d times\n",a[i],f[i]);
+            
+        }
+    return 0;
+}
