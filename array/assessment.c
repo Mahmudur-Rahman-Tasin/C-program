b271900d4f66ef7357d73e150bbfdbf9811d98e1
Mahmudur-Rahman-Tasin <mtasin2017@gmail.com>
2024-01-27 21:51:41 +0600

    Update assessment.c

--- array/assessment.c
+++ array/assessment.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    int x,i,y,n,j,t,x2,t2;
+    int x,i,y,n,t,x2,t2;
     scanf("%d",&x);//5
     double dem[x],sum;
     for(i=0;i<x;i++)
@@ -9,15 +9,16 @@ int main()
         scanf("%lf",&dem[i]);
     }
     scanf("%d %d",&y,&n);//4 3
-    for(t=1;t<=y;t++)                                    //next 4 week e 4 bar cholbe, y 1 theke 4
+    
+    for(t=1;t<=y;t++)                                    //next 4 week e 4 bar cholbe, y 1 upto 4
     {
-        for(x2=x,sum=0,t2=1;t2<=n;t2++,x2--)             //moving avg, n 1 theke 3 porjonto LAST 3TA diye ber korbo
+        for(x2=x,sum=0,t2=1;t2<=n;t2++,x2--)             //moving avg, n 1 upto 3 porjonto LAST 3TA diye ber korbo
         {
             sum=sum+dem[x2-1];
         }
-        printf("%lf ",sum/n);                            //left shift but shururta baad
+        printf("%lf ",sum/n);                            //demand for y=1 upto 4
         
-        for(i=0;i<x-1;i++)
+        for(i=0;i<x-1;i++)                               //left shift but shururta baad
         {
             dem[i]=dem[i+1];
         }
